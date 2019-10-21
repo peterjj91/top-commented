10c782229b254a248acf3214e5e4cd6cc765401f
Popeskul Pavel <peterjj91@gmail.com>
2019-10-21 23:09:44 +0300

    Grid for cards

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import UICard from './../UICard';
+import { Container, Row, Col } from 'reactstrap';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div className="">
-        <UICard img="s" title="asd" count={1} link="sd" />
-      </div>
+      <Container fluid={true}>
+        <Row>
+          <Col sm={4} md={3}>
+            <UICard img="s" title="asd" count={1} link="sd" />
+          </Col>
+          <Col sm={4} md={3}>
+            <UICard img="s" title="asd" count={1} link="sd" />
+          </Col>
+          <Col sm={4} md={3}>
+            <UICard img="s" title="asd" count={1} link="sd" />
+          </Col>
+          <Col sm={4} md={3}>
+            <UICard img="s" title="asd" count={1} link="sd" />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
